eb625dd7a4f6e7c9db7cb249f4dac4f115f7c2b6
simonrowlands <si_rhys96@hotmail.co.uk>
2019-01-14 10:50:21 +0000

    Observable examples

--- Observables.playground/Contents.swift
+++ Observables.playground/Contents.swift
@@ -5,4 +5,56 @@
 //
 
 import RxSwift
-//import RxCocoa
+
+enum MyError: Error {
+    case anError
+}
+
+var one = 1
+var two = 2
+var three = 3
+var numbers = [one, two, three]
+
+example(of: "just, of, from") {
+    
+    let just = Observable.just(one)
+    let of = Observable.of(one, two, three)
+    let ofArray = Observable.of(numbers)
+    let from = Observable.from(numbers)
+    
+    // NOTE: printMe is a custom extension on the Observable class. It uses the RxSwift `subscribe` function to print the elements which will be explained below
+    
+    just.printMe("Just")
+    of.printMe("Of")
+    ofArray.printMe("Of array")
+    from.printMe("From array")
+}
+
+example(of: "subscribe") {
+    
+    let observable = Observable.of(one, two, three)
+    
+    observable.subscribe(onNext: { element in
+        print(element)
+    })
+}
+
+example(of: "empty") {
+    let observable = Observable<Void>.empty()
+    
+    observable.subscribe(onNext: { element in
+        print(element)
+    }, onCompleted: {
+        print("Completed")
+    })
+}
+
+example(of: "never") {
+    let observable = Observable<Any>.never()
+    
+    observable.subscribe(onNext: { element in
+        print(element)
+    }, onCompleted: {
+        print("Completed")
+    })
+}
--- Observables.playground/Sources/Support.swift
+++ Observables.playground/Sources/Support.swift
@@ -1,9 +1,24 @@
 import Foundation
 
+import RxSwift
+
 public func example(of description: String, action: () -> Void) {
     print("""
     
     >>> Example of: \(description)
     """)
     action()
+    print("<<<")
+}
+
+extension Observable {
+    public func printMe(_ id: String? = nil) {
+        self.subscribe(onNext: { element in
+            if let id = id {
+                print("\(id): \(element)")
+            } else {
+                print(element)
+            }
+        }).disposed(by: DisposeBag())
+    }
 }
